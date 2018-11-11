4db735cf1542604f3e72ebda82f86cde07f80ed4
zijie <mzijiezhang@outlook.com>
2018-11-11 16:35:28 +0800

    update src

--- FGSM/data.py
+++ FGSM/data.py
@@ -1,11 +1,40 @@
 import torch
-
 from torchvision import datasets, transforms
 
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST("./datasets", train=False, download=True,
-    transform=transforms.Compose([
+import os
+
+
+input_size = 224
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize(input_size),
+        # transforms.RandomResizedCrop(input_size),
+        transforms.Grayscale(3),
         transforms.ToTensor(),
-        ])),
-    batch_size=1, shuffle=True)
-       
+        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(input_size),
+        # transforms.CenterCrop(input_size),
+        transforms.Grayscale(3),
+        transforms.ToTensor(),
+        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+}
+ 
+# train_datasets = datasets.ImageFolder(os.path.join(data_dir, "train"), data_transforms["train"])
+# test_datasets = datasets.ImageFolder(os.path.join(data_dir, "val"), data_transforms["val"])
+# 
+# train_loader = torch.utils.data.DataLoader(train_datasets, batch_size=16, shuffle=True)
+# test_loader = torch.utils.data.DataLoader(test_datasets, batch_size=1, shuffle=True)
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./datasets/', train=True, download=True, transform=data_transforms['train']),
+        batch_size=16, shuffle=True
+)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./datasets/', train=False, download=True, transform=data_transforms['val']),
+        batch_size=1, shuffle=True
+)
--- FGSM/main.py
+++ FGSM/main.py
@@ -1,34 +1,62 @@
-from model import *
 from data import *
+from model import *
+from train import *
 from test import *
 
 import matplotlib.pyplot as plt
+import numpy as np
 from PIL import Image
+
+import argparse
+
 
 plt.rcParams['figure.figsize'] = (6.0, 6.0)
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
+epsilons = [0, 0.05, 0.1, 0.2, 0.25, 0.3]
+accuracies = []
+model_name = "alexnet"
+num_classes = 10
+num_epochs = 10
+feature_extract = True
 path = "./examples/"
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", type=str)
+args = parser.parse_args()
+
 print("CUDA Available: ", torch.cuda.is_available())
-device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
+if str(device) == "cuda":
+    print("Using device: CUDA")
+else:
+    print("Using device: cpu")
 
+alexnet, input_size = initialize_alexnet(num_classes, feature_extract)
+# print(alexnet)
+alexnet = alexnet.to(device)
 
-lenet = LeNet().to(device)
-lenet.load_state_dict(torch.load(pretrained_model, map_location='cpu'))
-lenet.eval()
+params_to_update = []
+for name, param in alexnet.named_parameters():
+    if param.requires_grad == True:
+        params_to_update.append(param)
 
-accuracies, adv_exs = [], []
+if args.model == None:
+    optimizer = optim.SGD(params_to_update, lr=0.002, momentum=0.9)
+    alexnet_ft = train(alexnet, train_loader, optimizer, num_epochs=num_epochs)
+    torch.save(alexnet_ft.state_dict(), "./model_alexnet.pkl")
+else:
+    alexnet_ft = alexnet
+    alexnet_ft.load_state_dict(torch.load(args.model))
+
+test(alexnet_ft, test_loader)
+
 for eps in epsilons:
-    acc, ex = test(lenet, device, test_loader, eps)
+    acc, exs = attack(alexnet_ft, test_loader, eps)
     accuracies.append(acc)
-    adv_exs.append(ex)
-    if len(ex) != 0:
-        for i in range(len(ex)):
-            plt.imsave(path + "ex_" + str(eps) + "_" + str(i) + ".png", ex[i])
-    print("Adversarial examples saved to " + path)
- 
+    for i in range(len(exs)):
+        plt.imsave("./examples/" + str(eps) + "_" + str(i) + ".png", np.transpose(exs[i], (1, 2, 0)))
+
 
 plt.plot(epsilons, accuracies, "*-")
 plt.yticks(np.arange(0, 1.1, step=0.1))
